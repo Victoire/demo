c943f06fa575bdc648b05e1a2215909494219553
Leny Bernard <leny@appventus.com>
2015-04-13 03:13:40 +0200

    update deploy.rb

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -4,10 +4,10 @@
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
 
-set :application, "appventus.com"
+set :application, "demo.victoire.io"
 
 set :scm,         :git
-set :repository,  "ssh://git@stash.appventus.com:7999/app/appventus.com.git"
+set :repository,  "git@github.com:Victoire/demo.git"
 set :deploy_via,  :remote_cache
 
 ssh_options[:forward_agent] = true
@@ -17,13 +17,13 @@ set :use_composer,   true
 set :update_vendors, false
 set :composer_options, "--verbose --prefer-dist --optimize-autoloader"
 
-set :writable_dirs,     ["var/cache", "var/logs", "web/uploads"]
+set :writable_dirs,     ["var/cache", "var/logs", "web/media"]
 set :webserver_user,    "www-data"
 set :permission_method, :acl
 set :use_set_permissions, true
 
 set :shared_files,    ["app/config/parameters.yml", "web/.htaccess", "web/robots.txt"]
-set :shared_children, ["var/logs", "web/uploads", "app/sessions", "web/data", "web/wp-content"]
+set :shared_children, ["var/logs", "web/uploads", "app/sessions"]
 
 set :dump_assetic_assets, true
 set :model_manager, "doctrine"
@@ -35,29 +35,15 @@ set :symfony_console, "bin/console"
 set :keep_releases, 3
 
 # desc "Deploy to production"
-# task :prod do
-    set :domain,      "appventus.com"
-    set :deploy_to,   "/var/www/appventus/appventus.com"
-    set :user,        "nimba"
-    set :branch,      "develop"
-    set :deploy_via,  :remote_cache
+set :domain,      "demo.victoire.io"
+set :deploy_to,   "/var/www/demo.victoire.io"
+set :user,        "anakin"
+set :branch,      "master"
+set :deploy_via,  :remote_cache
 
-    role :web,        domain
-    role :app,        domain, :primary => true
-    role :db,         domain, :primary => true
-# end
-
-namespace :apache do
-  [:stop, :start, :restart, :reload].each do |action|
-    desc "#{action.to_s.capitalize} Apache"
-    task action, :roles => :web do
-      capifony_pretty_print "--> #{action.to_s.capitalize} Apache"
-
-      run "#{sudo} /etc/init.d/apache2 #{action.to_s}"
-      capifony_puts_ok
-    end
-  end
-end
+role :web,        domain
+role :app,        domain, :primary => true
+role :db,         domain, :primary => true
 
 namespace :victoire do
     desc "(re)generate victoire views"
@@ -80,14 +66,11 @@ namespace :bower do
 end
 
 before "deploy:finalize_update", "bower:install"
-
 after 'deploy' do
     symfony.doctrine.schema.update
 end
-
 after "deploy", "deploy:web:disable"
 after "deploy", "victoire:generate_views"
 after "deploy", "deploy:set_permissions"
-after "deploy", "apache:reload"
 after "deploy", "deploy:web:enable"
 after "deploy", "deploy:cleanup"
